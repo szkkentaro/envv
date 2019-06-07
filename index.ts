75facaff45ae3ac9b613bb52169dda4c0eed3329
Kentaro SUZUKI <kentarock@gmail.com>
2019-06-08 00:34:35 +0900

    feat: Reconnect mqtt client

--- index.ts
+++ index.ts
@@ -44,15 +44,6 @@ const region:string = argv.region;
 const publishInterval:number = 10000; // msec
 let intervalId:NodeJS.Timeout;
 
-// Make JWT token
-const payload = {
-    iat: parseInt((Date.now() / 1000).toString()),
-    exp: parseInt((Date.now() / 1000).toString()) + 20 * 60, // 20 minutes
-    aud: projectId,
-};
-const privateKey = fs.readFileSync('rsa_private.pem');
-const password = jwt.sign(payload, privateKey, { algorithm: 'RS256' })
-
 // Sensor Data Reader
 const reader = () => {
     return new Promise((resolve: any, reject: any) =>{
@@ -69,68 +60,86 @@ const reader = () => {
     });
 }
 
-// MQTT Client connect
-const params: mqtt.IClientOptions = {
-    host: 'mqtt.googleapis.com',
-    port: 8883,
-    clientId: `projects/${projectId}/locations/${region}/registries/${registryId}/devices/${deviceId}`,
-    username: 'unused',
-    password: password,
-    protocol: 'mqtts',
+const connect = () => {
+    // Make JWT token
+    const payload = {
+        iat: parseInt((Date.now() / 1000).toString()),
+        exp: parseInt((Date.now() / 1000).toString()) + 20 * 60, // 20 minutes
+        aud: projectId,
+    };
+    const privateKey = fs.readFileSync('rsa_private.pem');
+    const password = jwt.sign(payload, privateKey, { algorithm: 'RS256' })
+
+    // MQTT Client connect
+    const params: mqtt.IClientOptions = {
+        host: 'mqtt.googleapis.com',
+        port: 8883,
+        clientId: `projects/${projectId}/locations/${region}/registries/${registryId}/devices/${deviceId}`,
+        username: 'unused',
+        password: password,
+        protocol: 'mqtts',
+    }
+    const client: mqtt.Client =  mqtt.connect(params); 
+
+    // MQTT Client config
+    client.subscribe(`/devices/${deviceId}/config`, {qos: 1});
+    client.subscribe(`/devices/${deviceId}/commands/#`, {qos: 1});
+
+    // MQTT Events setting
+    client.on('connect', (packet: { returnCode: number; }) => {
+        if (packet.returnCode === 0) {
+            console.log("connected")
+        }
+    });
+    client.on('close', () => {
+        console.log('close')
+        client.end();
+
+        // reconnect
+        connect();
+    });
+    client.on('error', err => console.log('error', err));
+    client.on('message', (topic, message) => {
+        const payload = message.toString();
+        // config
+        // if (topic === '/devices/my-device/config') {
+        //     // If you want to implemente
+        // }
+        // commands
+        if (topic === '/devices/my-device/commands') {
+            client.emit(payload);
+        }
+    });
+    client.on('start', () => {
+        console.log('start')
+        const topic: string = `/devices/${deviceId}/events`;
+        const opts: mqtt.IClientPublishOptions = { qos: 1 };
+
+        intervalId = setInterval(async () => {
+            const message: any = await reader();
+            client.publish(topic, JSON.stringify(message), opts, (err, data) => {
+                console.log('published data: ', data);
+                if (err) {
+                    console.log(err);
+                    clearInterval(intervalId);
+                    client.end();
+                }
+            });
+        }, publishInterval);
+    });
+    client.on('stop', () => {
+        console.log('stop')
+        if (intervalId) {
+            clearInterval(intervalId);
+        }
+    });
+    client.on('end', () => {
+        console.log('end')
+        if (intervalId) {
+            clearInterval(intervalId);
+        }
+        client.end();
+    });
 }
-const client: mqtt.Client =  mqtt.connect(params); 
-
-// MQTT Client config
- client.subscribe(`/devices/${deviceId}/config`, {qos: 1});
-
- client.subscribe(`/devices/${deviceId}/commands/#`, {qos: 1});
-
-// MQTT Events setting
-client.on('connect', (packet: { returnCode: number; }) => {
-    if (packet.returnCode === 0) {
-        console.log("connected")
-    }
-});
-client.on('close', () => console.log('close'));
-client.on('error', err => console.log('error', err));
-client.on('message', (topic, message) => {
-    const payload = message.toString();
-    // config
-    // if (topic === '/devices/my-device/config') {
-    //     // If you want to implemente
-    // }
-    // commands
-    if (topic === '/devices/my-device/commands') {
-        client.emit(payload);
-    }
-});
-client.on('start', () => {
-    console.log('start')
-    const topic: string = `/devices/${deviceId}/events`;
-    const opts: mqtt.IClientPublishOptions = { qos: 1 };
-
-    intervalId = setInterval(async () => {
-        const message: any = await reader();
-        client.publish(topic, JSON.stringify(message), opts, (err, data) => {
-            console.log('published data: ', data);
-            if (err) {
-                console.log(err);
-                clearInterval(intervalId);
-                client.end();
-            }
-        });
-    }, publishInterval);
-});
-client.on('stop', () => {
-    console.log('stop')
-    if (intervalId) {
-        clearInterval(intervalId);
-    }
-});
-client.on('end', () => {
-    console.log('end')
-    if (intervalId) {
-        clearInterval(intervalId);
-    }
-    client.end();
-});
+// main
+connect();
